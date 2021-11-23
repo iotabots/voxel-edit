88168bda6e346ef2db9abfb41fbb7028228507a8
huhn511 <sehe89@gmail.com>
2021-11-24 00:01:40 +0100

    add iotabots style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,41 @@
 import "./App.css";
 import VoxelManager from "./VoxelManager";
+import Navigation from "./Navigation";
+import Footer from "./Footer";
+import { Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const App = () => {
-  return <VoxelManager />;
+
+  const theme = createTheme({
+    typography: {
+      fontFamily: 'Press Start 2P',
+    },
+    palette: {
+      primary: {
+        light: '#757ce8',
+        main: '#2b2b2b',
+        dark: '#002884',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#ff7961',
+        main: '#f44336',
+        dark: '#ba000d',
+        contrastText: '#000',
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+    <Navigation />
+      <Routes>
+        <Route path="/" element={<VoxelManager />} />
+      </Routes>
+      <Footer />
+    </ThemeProvider>
+  )
 };
 
 export default App;
